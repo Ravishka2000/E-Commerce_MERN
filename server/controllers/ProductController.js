ab275f506a491989888131e9eca3618b62524c5a
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-03-26 18:19:45 +0530

    create sort filter and pagination

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -36,7 +36,33 @@ const getAllProducts = asyncHandler (async (req, res) => {
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-        const query = Product.find(JSON.parse(queryStr));
+        let query = Product.find(JSON.parse(queryStr));
+
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(" ");
+            query = query.sort(sortBy);
+        }else{
+            query = query.sort("-createdAt");
+        }
+
+        if(req.query.fields){
+            const fields = req.query.fields.split(',').join(" ");
+            query = query.select(fields);
+        }else{
+            query = query.select("-__v")
+        }
+
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+
+        if (req.query.page){
+            const productCount = await Product.countDocuments();
+            if(skip >= productCount){
+                throw new Error("This Page does not exist");
+            }
+        }
 
         const products = await query; 
         res.json(products);
