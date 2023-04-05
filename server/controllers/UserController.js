d6e0ed43f0c323fa3260b73905a95664e28c8e8d
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-04-05 21:51:00 +0530

    create add to cart function

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,7 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/User.js";
+import Product from "../models/ProductModel.js";
+import Cart from "../models/CartModel.js";
 import bcrypt from "bcrypt";
 import generateToken from "../config/jwtToken.js";
 import generateRefreshToken from "../config/refreshToken.js";
@@ -323,6 +325,41 @@ const getWishlist = asyncHandler (async (req, res) => {
     }
 });
 
+const userCart = asyncHandler(async (req, res) => {
+    const { cart } = req.body;
+    const { _id } = req.user;
+    try {
+        let products = [];
+        const user = await User.findById(_id);
+        const alreadyExistCart = await Cart.findOne({ orderby: user._id });
+        if (alreadyExistCart) {
+            await alreadyExistCart.remove();
+        }
+        for (let i = 0; i < cart.length; i++) {
+            let object = {};
+            object.product = cart[i]._id;
+            object.count = cart[i].count;
+            object.color = cart[i].color;
+            let getPrice = await Product.findById(cart[i]._id).select("price").exec();
+            object.price = getPrice.price;
+            products.push(object);
+        }
+        let cartTotal = 0;
+        for (let i = 0; i < products.length; i++) {
+            cartTotal = cartTotal + products[i].price * products[i].count;
+        }
+        let newCart = await new Cart({
+            products,
+            cartTotal,
+            orderby: user?._id,
+        }).save();
+        res.json(newCart);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+
 export default {
     createUser,
     loginUser,
@@ -340,4 +377,5 @@ export default {
     forgotPasswordToken,
     resetPassword,
     getWishlist,
+    userCart,
 }
